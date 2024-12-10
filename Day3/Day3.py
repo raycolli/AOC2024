b1582e2c7998920f42e573b4902de7e0972586d2
raycolli <arrachelcollier@gmail.com>
2024-12-10 06:29:25 -0600

    day 4

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,1 +1,38 @@
 # im tired
+import re
+
+with open('input.txt', 'r') as f:
+    data = f.read()
+
+pattern = r'mul\(\d\d?\d?,\d\d?\d?\)'
+def get_result(s):
+    result = 0
+    for m in re.findall(pattern, s):
+        l, r = m.split(',')
+        result += int(l[4:])*int(r[:-1])
+    return result
+
+# part 1
+print(get_result(data))
+
+# part 2
+clean_data = []
+i = 0
+do = True
+while i < len(data):
+    if do:
+        if data[i:i+5] == 'don\'t':
+            do = False
+            i += 5
+        else:
+            clean_data.append(data[i])
+            i += 1
+    else:
+        if data[i:i+2] == 'do' and data[i:i+5] != 'don\'t':
+            do = True
+            i += 2
+        else:
+            i += 1
+clean_data = ''.join(clean_data)
+
+print(get_result(clean_data))
---
+++ Day4/Day4.py
@@ -0,0 +1,44 @@
+word_search = []
+with open('input.txt', 'r') as f:
+    for line in f.readlines():
+        word_search.append(list(line.strip()))
+n = len(word_search)
+
+def is_inbounds(i, j):
+    return 0 <= i < n and 0 <= j < n
+
+# part 1
+result = 0
+for i in range(n):
+    for j in range(n):
+        if word_search[i][j] != 'X':
+            continue
+        for di in [-1, 0, 1]:
+            for dj in [-1, 0, 1]:
+                if (di, dj) == (0, 0):
+                    continue
+                if is_inbounds(i+3*di, j+3*dj):
+                    if ''.join(word_search[i+k*di][j+k*dj] for k in range(4)) == 'XMAS':
+                        result += 1
+print(result)
+
+# part 2
+result = 0
+for i in range(n):
+    for j in range(n):
+        if word_search[i][j] != 'A':
+            continue
+        if not is_inbounds(i+1, j+1):
+            continue
+        if not is_inbounds(i+1, j-1):
+            continue
+        if not is_inbounds(i-1, j+1):
+            continue
+        if not is_inbounds(i-1, j-1):
+            continue
+        if not (word_search[i-1][j-1], word_search[i+1][j+1]) in (('M', 'S'), ('S', 'M')):
+            continue
+        if not (word_search[i+1][j-1], word_search[i-1][j+1]) in (('M', 'S'), ('S', 'M')):
+            continue
+        result += 1
+print(result)
