6b5c6dc6702fa2f9c5a43b28155e695718c7c313
raycolli <82118894+raycolli@users.noreply.github.com>
2024-12-13 19:27:47 -0600

    update day 11

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -1,40 +1,26 @@
-def split_number(num):
-    """Splits a number with an even number of digits into two halves."""
-    num_str = str(num)
-    mid = len(num_str) // 2
-    left = int(num_str[:mid])
-    right = int(num_str[mid:])
-    return left, right
+from functools import cache
 
-def simulate_stones(stones, blinks):
-    for _ in range(blinks):
-        new_stones = []
-        for stone in stones:
-            if stone == 0:
-                new_stones.append(1)
-            elif len(str(stone)) % 2 == 0:
-                left, right = split_number(stone)
-                new_stones.extend([left, right])
-            else:
-                new_stones.append(stone * 2024)
-        stones = new_stones
-    return len(stones)
+with open('input.txt', 'r') as f:
+    stones = [int(x) for x in f.readline().split()]
 
-# Load numbers from a text file
-def load_stones_from_file(filename):
-    with open(filename, 'r') as file:
-        content = file.read()
-        # Split the content into integers (assuming space or newline separation)
-        stones = list(map(int, content.split()))
-    return stones
+@cache
+def get_span(stone, num_blinks):
+    if num_blinks == 0:
+        return 1
+    if stone == 0:
+        return get_span(1, num_blinks-1)
+    elif len(str(stone)) % 2 == 0:
+        n = len(str(stone))
+        left = int(str(stone)[:n // 2])
+        right = int(str(stone)[n // 2:])
+        return get_span(left, num_blinks-1) + get_span(right, num_blinks-1)
+    else:
+        return get_span(2024*stone, num_blinks-1)
 
-# Main program
-filename = "input.txt"  # Replace with the path to your file
-initial_stones = load_stones_from_file(filename)
+# part 1
+num = 25
+print(sum(get_span(x, num) for x in stones))
 
-# Number of blinks
-blinks = 75
-
-# Calculate the number of stones after 25 blinks
-result = simulate_stones(initial_stones, blinks)
-print(f"Number of stones after {blinks} blinks: {result}")
+# part 2
+num = 75
+print(sum(get_span(x, num) for x in stones))
